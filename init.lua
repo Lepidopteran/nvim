b585837177a193807a7d7a4cbb87659f25e81c05
Blaine Nicholls <bnicholls@tstate.com>
2024-05-01 12:57:16 -0400

    feat: add undo file

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ local vim = vim
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
+
 vim.keymap.set('n', '<c-h>', '<c-w>h')
 vim.keymap.set('n', '<c-j>', '<c-w>j')
 vim.keymap.set("n", "<c-k>", "<c-w>k")

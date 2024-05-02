883ed2628002e1dbc04cd9f9864e234a3a933602
Blaine Nicholls <bnicholls@tstate.com>
2024-05-02 11:52:06 -0400

    feat: enable spellchecking on startup

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
 vim.opt.undolevels = 1000
 vim.opt.undoreload = 10000
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
 
 vim.keymap.set('n', '<c-h>', '<c-w>h')
 vim.keymap.set('n', '<c-j>', '<c-w>j')

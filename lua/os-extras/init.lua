a045ba89eb2fe6ab4d94ae38d3536394869531a2
Blaine <blainenicholls@proton.me>
2024-05-26 17:44:58 -0400

    refactor(os-extras): change powershell to pwsh

--- lua/os-extras/init.lua
+++ lua/os-extras/init.lua
@@ -2,7 +2,7 @@ local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
 if is_windows then
 	local aspnet_webforms_file_types = vim.api.nvim_create_augroup("aspnet_webforms", { clear = true})
-  vim.o.shell = "powershell"
+  vim.o.shell = "pwsh"
   vim.o.shellcmdflag = "-command"
   vim.o.shellquote = "\\"
   vim.o.shellxquote = ""

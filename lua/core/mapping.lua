a4980a95eff6fa0389711ac2a34e8388546c845a
Blaine Nicholls <bnicholls@tstate.com>
2024-07-25 14:18:17 -0400

    feat(core): add buffer shortcut

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -10,54 +10,64 @@ M.general = {
 		{ "<leader>x", '"+x', desc = "Cut to clipboard" },
 		{ "<leader>y", '"+y', desc = "Copy to clipboard" },
 	},
+
 	{ "p", "P", desc = "Paste without yanking", mode = "x" },
 }
 
 M.telescope = {
-	{"<leader>f", group="Telescope" },
+	{ "<leader>f", group = "Telescope" },
 	{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
 	{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
 	{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
 	{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "Old Files" },
 	{ "<leader>fh", "<cmd>Telescope frecency<CR>", desc = "Frecency" },
 	{ "<leader>fm", "<cmd>Telescope marks<CR>", desc = "Marks" },
-	{ "<leader>fs", group="Symbols" },
+	{ "<leader>fs", group = "Symbols" },
 	{ "<leader>fsd", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Document Symbols" },
 	{ "<leader>fsw", "<cmd>Telescope lsp_workspace_symbols<CR>", desc = "Workspace Symbols" },
-	{ "<leader>fl", group="LSP" },
+	{ "<leader>fl", group = "LSP" },
 	{ "<leader>fld", "<cmd>Telescope lsp_definitions<CR>", desc = "Definitions" },
 	{ "<leader>flr", "<cmd>Telescope lsp_references<CR>", desc = "References" },
 	{ "<leader>fli", "<cmd>Telescope lsp_implementations<CR>", desc = "Implementations" },
 	{ "<leader>flt", "<cmd>Telescope lsp_type_definitions<CR>", desc = "Type Definitions" },
-	{ "<leader>fe", group="Extra" },
+	{ "<leader>fe", group = "Extra" },
 	{ "<leader>fec", "<cmd>Telescope commands<CR>", desc = "Commands" },
 	{ "<leader>feh", "<cmd>Telescope help_tags<CR>", desc = "Help Tags" },
 	{ "<leader>fes", "<cmd>Telescope highlights<CR>", desc = "Highlights" },
 	{ "<leader>few", "<cmd>Telescope spell_suggest<CR>", desc = "Spell Suggest" },
-	{ "<leader>fr", group="Git" },
-	{ "<leader>frc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
-	{ "<leader>frb", "<cmd>Telescope git_branches<CR>", desc = "Branches" },
-	{ "<leader>frs", "<cmd>Telescope git_status<CR>", desc = "Status" },
-	{ "<leader>frf", "<cmd>Telescope git_files<CR>", desc = "Files" },
-	{ "<leader>ft", "<cmd>Telescope treesitter<CR>", desc = "Treesitter" },
+	{ "<leader>fr", group = "Git" },
+	{
+		{ "<leader>frc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
+		{ "<leader>frb", "<cmd>Telescope git_branches<CR>", desc = "Branches" },
+		{ "<leader>frs", "<cmd>Telescope git_status<CR>", desc = "Status" },
+		{ "<leader>frf", "<cmd>Telescope git_files<CR>", desc = "Files" },
+		{ "<leader>ft", "<cmd>Telescope treesitter<CR>", desc = "Treesitter" },
+	}
 }
 
 M.buffer = {
-	{ "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Find Buffer" },
+	{ "<leader>bs", "<cmd>Telescope buffers<cr>", desc = "Find Buffer" },
 	{ "<leader>bc", "<cmd>bdelete<cr>", desc = "Close Buffer" },
 	{ "<leader>bn", "<cmd>bnext<cr>", desc = "Next Buffer" },
 	{ "<leader>bp", "<cmd>bprevious<cr>", desc = "Previous Buffer" },
+	{
+		"<leader>bb",
+		group = "buffers",
+		expand = function()
+			return require("which-key.extras").expand.buf()
+		end,
+	},
 }
 
 M.nvim_tree = {
-	{ "<leader>e", group="file" },
+	{ "<leader>e", group = "file" },
 	{ "<leader>ee", "<cmd>NvimTreeToggle<CR>", desc = "Toggle" },
 	{ "<leader>er", "<cmd>NvimTreeRefresh<CR>", desc = "Refresh" },
 	{ "<leader>ef", "<cmd>NvimTreeFindFile<CR>", desc = "Find File" },
 }
 
 M.lsp = {
-	{ "<leader>l", group="lsp"},
+	{ "<leader>l", group = "lsp" },
 	{ "<leader>lK", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "Hover" },
 	{ "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<CR>", desc = "Code Action" },
 	{ "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<CR>", desc = "Go to Definition" },
@@ -66,11 +76,11 @@ M.lsp = {
 }
 
 M.formatter = {
-	{"<leader>lf", "<cmd>lua require('conform').format()<CR>", desc = "Format" },
+	{ "<leader>lf", "<cmd>lua require('conform').format()<CR>", desc = "Format" },
 }
 
 M.oil = {
-	{"<leader>eo","<cmd>Oil --float<CR>", desc = "Open Oil" },
+	{ "<leader>eo", "<cmd>Oil --float<CR>", desc = "Open Oil" },
 }
 
 M.gitsigns = {

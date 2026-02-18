1e8cb79292498e2a160d96cdfe6386c4099c5725
Blaine <blainenicholls@protonmail.com>
2026-02-18 15:16:58 -0500

    chore(extra): remove sql development plugins

--- lua/extra/plugins.lua
+++ lua/extra/plugins.lua
@@ -128,35 +128,6 @@ local plugins = {
 		end,
 	},
 
-	-- SQL Development
-
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBuffer",
-		},
-		init = function()
-			vim.g.db_ui_use_nerd_fonts = 1
-		end,
-	},
-	{
-		"saghen/blink.cmp",
-		opts = {
-			sources = {
-				default = { "dadbod" },
-				providers = {
-					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-				},
-			},
-		},
-	},
 
 	-- Blender Addon Development
 

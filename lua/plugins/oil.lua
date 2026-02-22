7ab0d9d58079fb79f003d46fc2653f7f89109407
Blaine <blainenicholls@protonmail.com>
2026-02-22 17:12:54 -0500

    refactor(oil): configure

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,7 +6,18 @@ return {
 		dependencies = { "echasnovski/mini.icons" },
 		---@module "oil"
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+			delete_to_trash = true,
+			watch_for_changes = true,
+			skip_confirm_for_simple_edits = true,
+			lsp_file_methods = {
+				autosave_changes = "unmodified",
+			},
+			float = {
+				padding = 4,
+				border = "rounded",
+			}
+		},
 		lazy = false,
 		init = function()
 			wk.add({

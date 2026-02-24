9abb48cc324127186a20ba9f259cb7b3f568c74d
Blaine <blainenicholls@protonmail.com>
2026-02-24 12:26:18 -0500

    feat: add toggle term plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
+	version = "*",
+	opts = {
+		open_mapping = [[<c-\>]],
+		direction = "float",
+		float_opts = {
+			border = "rounded",
+			winblend = 3,
+			highlights = {
+				border = "Normal",
+				background = "Normal",
+			},
+		},
+	},
+}

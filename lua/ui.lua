6d2b87556f97f7af367968c9fb18d424ded24bdc
Blaine <blainenicholls@protonmail.com>
2026-02-23 21:04:38 -0500

    refactor(ui): set theme to kanagawa

--- lua/ui.lua
+++ lua/ui.lua
@@ -4,8 +4,9 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		lazy = false,
+		priority = 1000,
 		init = function()
-			vim.cmd("colorscheme kanagawa-dragon")
+			vim.cmd("colorscheme kanagawa")
 		end,
 	},
 	{
